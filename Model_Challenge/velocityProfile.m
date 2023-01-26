5769cfd29b5dc104ed1204fa6d31e5a49871e90c
Ffwrightson <123338832+Ffwrightson@users.noreply.github.com>
2023-01-25 16:02:55 -0800

    update datum
    update datum input

--- Model_Challenge/velocityProfile.m
+++ Model_Challenge/velocityProfile.m
@@ -1,6 +1,6 @@
-function w = velocityProfile(J, t_s, w_nl)
+function w = velocityProfile(J, t_s, w_tv)
     % Return velocity profile of motor with J inertia, t_s stall torque,
     % and w_nl no load speed
-    tr = w_nl * J / t_s;
-    w = @(t) w_nl * (1 - exp(-t/tr)) / (2*pi) * 60;
+    tr = w_tv * J / t_s;
+    w = @(t) w_tv * (1 - exp(-t/tr)) / (2*pi) * 60;
 end
