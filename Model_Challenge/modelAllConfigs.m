3dbcbc34365157e0ed217f2aa1d11367fab4fd43
Parker <pknopf@ucsd.edu>
2023-01-30 19:03:54 -0800

    fixed s mean

--- Model_Challenge/modelAllConfigs.m
+++ Model_Challenge/modelAllConfigs.m
@@ -1,17 +1,17 @@
 clear all; close all; clc;
 
-config = [[8, 2]; [4, 1]; [2, 1]];
+config = [[8, 2]; [4, 1]; [2, 1]; [4, 2]];
 pwm = [100, 75, 50];
 
-subplot(3,3,1)
+subplot(length(config),length(pwm),1)
 
-s = zeros(length(config)*length(pwm));
+s = zeros(length(config), length(pwm));
 for i = 1:length(config)
     for j = 1:length(pwm)
 
-        subplot(3,3, 3*(i-1) + j)
+        subplot(length(config),length(pwm), length(pwm)*(i-1) + j)
 
-        s(i) = modelConfig(config(i, 1), config(i, 2), pwm(j));
+        s(i,j) = modelConfig(config(i, 1), config(i, 2), pwm(j));
     end 
 end
 
