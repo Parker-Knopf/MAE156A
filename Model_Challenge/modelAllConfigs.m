7fcaba82616f610c49d317ff20eace0d48baa54a
Ffwrightson <123338832+Ffwrightson@users.noreply.github.com>
2023-02-01 22:32:08 -0800

    Improving Sim V2

--- Model_Challenge/modelAllConfigs.m
+++ Model_Challenge/modelAllConfigs.m
@@ -8,11 +8,19 @@ plot = false;
 if (plot)
     subplot(length(config),length(pwm),1)
 end
-jma = linspace(4.5*10^(-7), 4.5*10^(-7), 1);
-mewa = linspace(.16,.16,1);
-cda = linspace(25, 25, 1);
+jma = linspace(3.5*10^(-7), 5.5*10^(-7), 20);
+mewa = linspace(.11,.20,20);
+cda = linspace(24, 33, 20);
+
+%cda=cda(14);
+%mewa=mewa(1);
+%jma=jma(5);
+
+tot=length(mewa)*length(cda)*length(jma);
+
 o=1;
 p=1;
+indx=zeros(tot,3);
 s = zeros(length(config), length(pwm));
 for i = 1:length(config)
     for j = 1:length(pwm)
@@ -26,7 +34,8 @@ for i = 1:length(config)
                     mew=mewa(l);
                     Cad=cda(k);
 
-                    s(p,o) = modelConfig(config(i, 1), config(i, 2), pwm(j), plot,mew,Jm,Cad)
+                    s(p,o) = modelConfig(config(i, 1), config(i, 2), pwm(j), plot,mew,Jm,Cad);
+                    indx(p,:)=[m,l,k];
                     p=p+1;
                 end
             end
@@ -39,10 +48,13 @@ end
 if (plot)
     sgtitle("All Configurations: Model vs Results")
 end
-% for i=1:1000
-%     b(i) = mean(s(i,:));
-% end
+for i=1:tot
+    b(i) = mean(s(i,:));
+end
+[M,I]=min(b)
+c=[indx(I,:)];
+CDnew=cda(c(3))
+Mewnew=mewa(c(2))
+Jmnew=jma(c(1))
 
-
-
-%fprintf("Model Accuracy: %.3f\n", b);
+fprintf("Model Accuracy: %.3f\n", M);
--- Model_Challenge/odefcn.m
+++ Model_Challenge/odefcn.m
@@ -2,5 +2,5 @@ function dydt = odefcn(t, y, J, t_s, w_tv, n, n_b, n_n,mew,Cad)
     % Return numerical solution to be used with ODE45
     dydt = zeros(2,1);
     dydt(1) = y(2);
-    dydt(2) = (1 / J) * ((t_s *(1 - y(2)/w_tv)) - torqueDrag(y(2), n, n_b, n_n,mew) - stallTorqueFriction(n, n_b, n_n,Cad));
+    dydt(2) = (1 / J) * ((t_s *(1 - y(2)/w_tv)) - torqueDrag(y(2), n, n_b, n_n,Cad) - stallTorqueFriction(n, n_b, n_n,mew));
 end
