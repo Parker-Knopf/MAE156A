d24870eb9916a6c7ca3e150f00baa8654499e892
Parker Knopf <parker.knopf@gmail.com>
2023-02-09 10:18:00 -0800

    remove legacy code

--- Model_Challenge/velocityProfile.m
+++
@@ -1,6 +0,0 @@
-function w = velocityProfile(J, t_s, w_tv)
-    % Return velocity profile of motor with J inertia, t_s stall torque,
-    % and w_nl no load speed
-    tr = w_tv * J / t_s;
-    w = @(t) w_tv * (1 - exp(-t/tr)) / (2*pi) * 60;
-end
