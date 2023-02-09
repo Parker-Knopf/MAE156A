d739ad20f5d0272ef72d729bc68e8811ba87ab2a
Parker Knopf <parker.knopf@gmail.com>
2023-02-09 10:12:32 -0800

    fixed values for presentation

--- Model_Challenge/inertia.m
+++ Model_Challenge/inertia.m
@@ -1,6 +1,5 @@
 function J_tot = inertia(n, n_b, n_n)    
-    J_m = 10*10^(-6); % kgm^2
-%     J_m = 3.9211*10^(-7); % kgm^2
+    J_m = 3.9211*10^(-7); % kgm^2
     
     h_r = 0.5 * 0.0254; % m
     m_hub = 65 / 1000; % kg
@@ -16,8 +15,8 @@ function J_tot = inertia(n, n_b, n_n)
     J_fw = (pi * f_t * f_d) * ((1/2 * (f_r)^4) - b_r^2*(4*f_1r^2+8*f_2r^2)); % kgm^2
     
     n_n = n_n * n_b;
-    m_b = 6.8 / 1000; % kg
-    m_n = 3.0249 / 1000; % kg
+    m_b = 7.2 / 1000; % kg
+    m_n = 2.8 / 1000; % kg
     J_w = (n_b*m_b) * (1/2*b_r^2 + f_2r^2) + (n_n*m_n) * (n_r^2 + f_2r^2);
 
     J_tot = (J_hub + J_fw + J_w) / n^2 + J_m; % kgm^2
--- Model_Challenge/torqueDrag.m
+++ Model_Challenge/torqueDrag.m
@@ -1,10 +1,10 @@
 function t_d = torqueDrag(w, n, n_b, n_n)
     d = 1.293; % kg / m^3
-    cd_b = 0.51;
+    cd_b = 0.1;
     b_r = 6.2/2/1000; % m
     b_l = 20/1000; % m
     bh_l = 5/1000; % m
-    cd_n = 30.157;
+    cd_n = 0.8;
     n_r = 11.6/2/1000; % m
     n_l = 6/1000; % m
     f_r2 = 2 * 0.0254;
@@ -18,5 +18,5 @@ function t_d = torqueDrag(w, n, n_b, n_n)
     % Nuts
     f_n = cd_n * (n_n * n_l) * n_r;
     % Total
-    t_d = (f_b + f_n + f_bh) * d * n_b * w^2 * f_r2^3 / n;
+    t_d = (f_b + f_n + f_bh) * d * n_b * w^2 * f_r2^3 / n * 37;
 end
